d718d9b4b8c43fb4af58a6154d844d7c60c19425
KaMeHb-UA <marlock@etlgr.com>
2019-05-01 00:40:07 +0300

    simple tests for fibonacci generator

--- qrScanner/index.js
+++ qrScanner/index.js
@@ -1,9 +1,34 @@
 import reader from './reader.js'
 
-console.log({reader});
+// tests for fibonacci generator
+document.body.innerHTML = '<pre style="width:100%;height:100%;overflow:auto;"></pre>';
+const pre = document.body.children[0];
+function echo(text){
+    pre.innerText += text + '\n'
+}
 
-export default class {
-    static async read(bitmap){
-        return (await qrReadF)(bitmap)
-    }
+setTimeout(async () => {
+    echo('starting tests...');
+    echo('sync stage:');
+    echo(await test1());
+    echo('threaded stage:');
+    echo(await test2());
+}, 2000)
+
+async function test1(){
+    const start = new Date;
+    const testStack = [];
+    for(let i = 0; i < 400; i++) testStack.push(() => reader(200));
+    for(let i = 0; i < testStack.length; i++) await testStack[i]();
+    const end = new Date;
+    return end - start
+}
+
+async function test2(){
+    const start = new Date;
+    const testStack = [];
+    for(let i = 0; i < 400; i++) testStack.push(reader(200));
+    await Promise.all(testStack);
+    const end = new Date;
+    return end - start
 }
